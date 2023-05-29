b8de0d01110b11faf92691bc9f338c849b46fd9d
Jedidiah Carl O. Tan <masterjed7262@gmail.com>
2023-05-29 10:20:58 +0800

    Add initial styles

--- 01-birthday-buddy/starter/src/App.jsx
+++ 01-birthday-buddy/starter/src/App.jsx
@@ -1,4 +1,41 @@
+import Data from "./data";
+
+const Person = ({ id, name, age, image }) => {
+  return (
+    <div className="person">
+      <img className="img" src={image} alt="" />
+      <div>
+        <h4>{name}</h4>
+        <p>{age} years</p>
+      </div>
+    </div>
+  );
+};
+
 const App = () => {
-  return <h2>Birthday Reminder - Starter</h2>;
+  return (
+    <main>
+      <div className="container">
+        <h3>5 Birthdays Today</h3>
+        {Data.map((person) => {
+          return (
+            <Person
+              id={person.id}
+              name={person.name}
+              age={person.age}
+              image={person.image}
+            />
+          );
+        })}
+        <Person
+          id={Data[0].id}
+          name={Data[0].name}
+          age={Data[0].age}
+          image={Data[0].image}
+        />
+        <button className="btn btn-block">Clear All</button>
+      </div>
+    </main>
+  );
 };
 export default App;
